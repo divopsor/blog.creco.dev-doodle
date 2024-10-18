a8bf3172e06fbe6df4d313e6d3965d7ed5748f42
dp <126376945+divopsor@users.noreply.github.com>
2024-10-18 16:54:08 +0900

    Update [id]/index.tsx

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -4,9 +4,15 @@ import { CATEGORY } from '../../src/constants';
 import { DetailsPage } from '../../src/containers/DetailsPage';
 import NotFoundPage from '../../src/containers/NotFoundPage';
 
-export const getStaticPaths = async () => getCrecoAppStaticPaths({ category: CATEGORY });
+export const getStaticPaths = async () => getCrecoAppStaticPaths({
+  category: CATEGORY,
+  prefix: "/github-api/api/gist",
+});
 
-export const getStaticProps: GetStaticProps = async (context) => getCrecoAppStaticProps(context, { category: CATEGORY });
+export const getStaticProps: GetStaticProps = async (context) => getCrecoAppStaticProps(context, {
+  category: CATEGORY,
+  prefix: "/github-api/api/gist"
+});
 
 export default function ({ item }: { item?: { id: string; body?: { contents: string; createdAt: number; updatedAt: number; }}}) {
   if (item == null) {
